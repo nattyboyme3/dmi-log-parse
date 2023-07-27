147bafb2e9ff484f3153333aa8d57e98a84ca831
Nat Biggs <nbiggs112@cedarville.edu>
2023-07-27 16:52:10 -0400

    adds email argument

--- parse.py
+++ parse.py
@@ -7,8 +7,6 @@ from dmi_parser import DMIParser
 import logging
 import argparse
 
-NOTIFICATION_EMAILS = ['howders@cedarville.edu','nbiggs112@cedarville.edu']
-
 
 def main(filename: str, min_to_check, show_stats: bool, debug:  bool):
     p = DMIParser(debug)
@@ -45,6 +43,7 @@ if __name__ == "__main__":
     a.add_argument('-m', '--minutes', type=int, help="how many minutes of log to parse", default=5)
     a.add_argument('-r', '--restart', type=int, help="how many errors will trigger a DMI restart", default=10)
     a.add_argument('-f', '--file', type=str, help="logfile to parse", default='dmi.log')
+    a.add_argument('-e', '--email', type=str, action='append', help="people to email", default=None)
     args = a.parse_args()
     filepath = args.file
     pending_max = args.restart
@@ -60,6 +59,7 @@ if __name__ == "__main__":
     if not args.debug and len(pending_issues) >= pending_max:
         log.info(f"found at least {pending_max} pending transactions. Sending emails and restarting live_ui listener")
         result = os.system("/usr/local/bin/restart_listener.sh live_ui_test")
-        for email in NOTIFICATION_EMAILS:
-            os.system(f"echo 'Dmi-log-parser restarted the Live_UI listener at {dt.now()} with result: {result}'"
-                      f" | mail -s 'dmi-log-parser restarted live_ui listener' {email}")
+        if args.email:
+            for email in args.email:
+                os.system(f"echo 'Dmi-log-parser restarted the Live_UI listener at {dt.now()} with result: {result}'"
+                          f" | mail -s 'dmi-log-parser restarted live_ui listener' {email}")
